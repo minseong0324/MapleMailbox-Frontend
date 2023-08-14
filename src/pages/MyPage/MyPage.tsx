ca56fe39ebfb94ad45c67f266d77d965ec5df799
minseong0324 <kwan0324@naver.com>
2023-08-15 00:16:53 +0900

    탈퇴하기 api

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import { s } from './style';
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 import Modal from '../../components/Modal/Modal';
 import BackButton from "src/components/BackButton/BackButton";
+
+const userId = localStorage.getItem("userId")
+const accessToken = localStorage.getItem("accessToken")
+
 
 function MyPage() {
     const [isLeaveModalOpen, setLeaveModalOpen] = useState(false);
@@ -14,16 +19,43 @@ function MyPage() {
         navigate('/select-character-tree');
     }
 
-    //탈퇴하기 함수
-    const handleSubmitLeave = () => { 
+    //탈퇴하기 모달창 열기 함수
+    const handleSubmitLeaveModalOpen = () => { 
         setLeaveModalOpen(true);
     }
 
      //취소 함수
     const handleSubmitCancel = () => { 
-
+        setLeaveModalOpen(false);
     }
 
+    //탈퇴하기 버튼 함수
+    const handleSubmitLeave = () => { 
+        axios.put(
+            `http://localhost:8080/auth/leave`,
+            userId,
+            {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                },
+            }
+        )
+        .then((response) => {
+            // 추가: response가 정의되어 있고 data가 있는지 확인
+            if(response.status === 200) {
+                // User has been deactivated, handle this (e.g. log out)
+                setLeaveModalOpen(false);
+                localStorage.removeItem("userId");
+                localStorage.removeItem("accessToken");
+                localStorage.removeItem("refreshToken");
+                window.location.reload(); // 페이지 새로고침
+            } else { // 에러 로직 추가해야함.
+                if (response.data.code === 'LEAVE MAPLEMAILBOX FAILED') {
+                    alert("계정을 탈퇴하는 데에 실패했습니다.")
+                }
+            }
+        })
+    }
 
     return (
         <>
@@ -32,7 +64,7 @@ function MyPage() {
             <s.TitleTextStyle>{userName}의 마이페이지</s.TitleTextStyle>
             <s.ButtonWrapper>
                 <s.Button onClick={handleNavigateSelectCharacterTree}>내 나무/캐릭터 변경하기</s.Button>
-                <s.Button onClick={handleSubmitLeave}>내 나무 없애기</s.Button>       
+                <s.Button onClick={handleSubmitLeaveModalOpen}>내 나무 없애기</s.Button>       
             </s.ButtonWrapper>
            
             <Modal isOpen={isLeaveModalOpen} onClose={() => setLeaveModalOpen(false)}>
