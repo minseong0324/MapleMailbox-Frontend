1073df5501bb1bb22e1c5b5f4811e4b8546fa48d
minseong0324 <65233075+minseong0324@users.noreply.github.com>
2023-08-23 21:50:36 +0900

    Delete VisitorMenu.d.ts

--- src/components/VisitorMenu/VisitorMenu.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface MenuProps {
-    onLogout: () => void;
-    onServiceDescription: () => void;
-}
-declare const VisitorMenu: React.FC<MenuProps>;
-export default VisitorMenu;
