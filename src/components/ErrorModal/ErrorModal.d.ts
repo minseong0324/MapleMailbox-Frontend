33e8c546b0de0cd8a5b159aec78228f40c3eeb04
minseong0324 <65233075+minseong0324@users.noreply.github.com>
2023-08-23 21:48:30 +0900

    Delete ErrorModal.d.ts

--- src/components/ErrorModal/ErrorModal.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-interface ModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    children: React.ReactNode;
-}
-declare const Modal: React.FC<ModalProps>;
-export default Modal;
