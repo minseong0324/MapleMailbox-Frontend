7ecc5e9f2a16f248b3acb94776404230337dda68
minseong0324 <65233075+minseong0324@users.noreply.github.com>
2023-08-23 21:50:11 +0900

    Delete ServiceModal.d.ts

--- src/components/ServiceModal/ServiceModal.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface ServiceModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-}
-declare const ServiceModal: React.FC<ServiceModalProps>;
-export default ServiceModal;
