112596685f6b7739221651d9586be13ba0dbaa7c
minseong0324 <65233075+minseong0324@users.noreply.github.com>
2023-08-23 22:07:51 +0900

    Delete useOnClickOutside.d.ts

--- src/hooks/useOnClickOutside.d.ts
+++
@@ -1,4 +0,0 @@
-import { RefObject } from "react";
-type Event = MouseEvent | TouchEvent;
-declare const useOnClickOutside: (ref: RefObject<HTMLElement>, handler: (event: Event) => void) => void;
-export default useOnClickOutside;
