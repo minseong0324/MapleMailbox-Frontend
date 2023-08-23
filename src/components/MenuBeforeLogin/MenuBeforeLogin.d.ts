815b88b0cd87cbb85a10cb298bdb024756faa02e
minseong0324 <65233075+minseong0324@users.noreply.github.com>
2023-08-23 21:49:05 +0900

    Delete MenuBeforeLogin.d.ts

--- src/components/MenuBeforeLogin/MenuBeforeLogin.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface MenuBeforeLoginProps {
-    onLogin: () => void;
-    onServiceDescription: () => void;
-}
-declare const MenuBeforeLogin: React.FC<MenuBeforeLoginProps>;
-export default MenuBeforeLogin;
