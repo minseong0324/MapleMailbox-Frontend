066d32417d5d3fd155835d20e5ab63639325100d
minseong0324 <65233075+minseong0324@users.noreply.github.com>
2023-08-23 21:48:47 +0900

    Delete Menu.d.ts

--- src/components/Menu/Menu.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-interface MenuProps {
-    onLogout: () => void;
-    onServiceDescription: () => void;
-    nowDate: number | null;
-}
-declare const Menu: React.FC<MenuProps>;
-export default Menu;
