1d6841f7a7b5bb0ace0e8fe67bb9b4a5adf32a33
minseong0324 <65233075+minseong0324@users.noreply.github.com>
2023-08-23 22:07:03 +0900

    Delete UserProvider.d.ts

--- src/contexts/UserProvider/UserProvider.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-type UserProviderProps = {
-    children: React.ReactNode;
-};
-declare function UserProvider({ children }: UserProviderProps): import("react/jsx-runtime").JSX.Element;
-export default UserProvider;
