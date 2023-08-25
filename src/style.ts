e526a9fcb92f256dd091755cdcbcc24393a8e0ca
minseong0324 <kwan0324@naver.com>
2023-08-25 12:52:11 +0900

    leafFalling 떨어진 위치 깜빡임 해결

--- src/style.ts
+++ src/style.ts
@@ -225,7 +225,7 @@ export const BackgroundAfterPC = styled.div`
   background: url(${bodyAfterBackground});
   background-position: center;
   background-repeat: no-repeat;
-  z-index: 1;
+  z-index: 2;
   -ms-interpolation-mode: nearest-neighbor;
   image-rendering: pixelated;
   background-size: 820px 1180px;
