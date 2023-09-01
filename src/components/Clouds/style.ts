9571086010114c7970b50ee16f453d09d7e86d32
minseong0324 <kwan0324@naver.com>
2023-09-01 19:53:59 +0900

    구름 위치 수정

--- src/components/Clouds/style.ts
+++ src/components/Clouds/style.ts
@@ -1,6 +1,7 @@
 import styled, { keyframes } from 'styled-components';
 import cloudImageSmall from '../../assets/cloud/clouds-small.png';
 import cloudImageBig from '../../assets/cloud/clouds-big.png';
+import { isMobile } from 'react-device-detect';
 
 const moveCloud = keyframes`
   0% {
@@ -26,16 +27,63 @@ const CloudsDiv = styled.div`
   }
 `;
 
+const mobileStyles = `
+  /* 모바일 스타일 */
+
+  /* 모바일 모든 크기 가로모드용 스타일 */
+  @media screen and (max-width: 999px) and (orientation: landscape) {
+    top: -25%;
+    height: 800px;
+  }
+
+  /* 태블릿 모든 크기 가로모드용 스타일 */
+  @media screen and (min-width: 1000px) and (orientation: landscape) {
+    top: -10%;
+    height: 800px;
+  }
+
+  /* 태블릿 미니, 기본 세로모드용 스타일 */
+  @media screen and (max-width: 820px) and (orientation: portrait) {
+    top: -5%;
+    height: 800px;
+  }
+
+  /* 큰 태블릿 세로모드용 스타일 */
+  @media screen and (min-width: 821px) and (orientation: portrait) {
+    top: -5%;
+    height: 800px;
+  }
+
+  /* 모바일 기본 세로모드용 스타일 */
+  @media screen and (max-width: 599px) and (orientation: portrait) {
+    top: 0%;
+    height: 800px;
+  }
+
+  /* 모바일 미니 세로모드용 스타일 */
+  @media screen and (max-width: 379px) and (orientation: portrait) {
+    top: 0%;
+    height: 600px;
+  }
+
+`;
+
 const WrapperDiv = styled.div`
   position: absolute;
   width: 100%;
   max-width: 820px;
-  height: 100vh;
+  height: 1000px;
   left: 50%;
   right: 50%;
+  top: 0%;
   transform: translateX(-50%);
   overflow: hidden;
   z-index: 0;
+  ${isMobile ? mobileStyles : ''}
+
+  @media screen and (min-width: 560px) {
+    top: -7%;
+  }
 `;
 
 export const s = {
